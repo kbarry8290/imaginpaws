b0379d935495bdd431f603afee02b2a84ae43c6f
kbarry8290 <110359860+kbarry8290@users.noreply.github.com>
2025-06-07 11:47:28 -0400

    Initial commit

--- app.config.ts
+++ app.config.ts
@@ -5,8 +5,12 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "ImaginPaws",
   slug: "ImaginPaws",
   extra: {
+    ...config.extra, // ← Keep any existing values!
     EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
     EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+    eas: {
+      projectId: "0d146bd6-645b-4257-b083-e8b4ff22e10f"
+    }
   },
   plugins: [
     [
